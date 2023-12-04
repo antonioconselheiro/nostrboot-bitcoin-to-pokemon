fb95c6c65b6bc9131908caaf86165531dbf6d082
António Conselheiro <canudos.sudestino@gmail.com>
2023-12-04 19:34:21 -0300

    incluindo publicaçõeso de pokémon comparando com o indice do ouro

--- packages/btc2poke/src/bitcoin-price.resultset.ts
+++ packages/btc2poke/src/bitcoin-price.resultset.ts
@@ -3,7 +3,8 @@ export interface BitcoinPriceResultset {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   readonly asset_id_base: 'BTC',
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  readonly asset_id_quote: 'USD',
-  // format example: 27933.7491211105
+  readonly asset_id_quote: 'USD' | 'XAUT',
+  // USD format example: 27933.7491211105
+  // XAUT format example: 20.618489926641793730922817107
   readonly rate: number
 }
--- packages/btc2poke/src/publish-memory.service.ts
+++ packages/btc2poke/src/publish-memory.service.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import { PostContentStatus } from './post-content-status.enum';
+import { IRecentPublishedMemory } from './recent-published-memory.interface';
 
 export class PublishMemoryService {
   static getInstance(): PublishMemoryService {
@@ -13,7 +14,10 @@ export class PublishMemoryService {
   private static instance: PublishMemoryService | null = null;
 
   private filename = process.env['APP_JSON_FILE_DATABASE'] || 'default.json';
-  private memory: (number | null)[] = [];
+  private memory: IRecentPublishedMemory = {
+    dolar: [],
+    gold: []
+  };
   private constructor() {
     this.loadFile();
   }
@@ -27,28 +31,30 @@ export class PublishMemoryService {
     }
   }
 
-  register(pokeid: number | null): void {
-    this.memory.shift();
-    this.memory.push(pokeid);
+  register(pokeid: number | null, asset: 'dolar' | 'gold'): void {
+    this.memory[asset].shift();
+    if (pokeid) {
+      this.memory[asset].push(pokeid);
+    }
     fs.writeFileSync(this.filename, JSON.stringify(this.memory));
   }
 
-  getStatus(pokeid: number | null): PostContentStatus {
-    const lastPublishIndex = this.memory.length - 1;
-    if (!this.memory.length) {
+  getStatus(pokeid: number | null, asset: 'dolar' | 'gold'): PostContentStatus {
+    const lastPublishIndex = this.memory[asset].length - 1;
+    if (!this.memory[asset].length) {
       return PostContentStatus.NOT_PUBLISHED_RECENTLY;
-    } else if (this.memory[lastPublishIndex] === pokeid) {
+    } else if (this.memory[asset][lastPublishIndex] === pokeid) {
       return PostContentStatus.LAST_POST;
-    } else if (this.memory[0] === pokeid) {
+    } else if (this.memory[asset][0] === pokeid) {
       return PostContentStatus.RECENT_POST;
     }
 
     return PostContentStatus.NOT_PUBLISHED_RECENTLY;
   }
 
-  hasAscended(newPokeId: number): boolean | null {
-    const lastPublishIndex = this.memory.length - 1;
-    const lastValue = this.memory[lastPublishIndex];
+  hasAscended(newPokeId: number, asset: 'dolar' | 'gold'): boolean | null {
+    const lastPublishIndex = this.memory[asset].length - 1;
+    const lastValue = this.memory[asset][lastPublishIndex];
     if (lastValue === null) {
       return null;
     }
