9525b2c31d05fa0896acfab4eae63ad22f63c071
António Conselheiro <anarcanudos@proton.me>
2024-10-06 16:39:53 -0300

    removing unused code

--- packages/btc2poke/src/relays.service.ts
+++ packages/btc2poke/src/relays.service.ts
@@ -1,15 +1,12 @@
-import { Event, getEventHash, getSignature, nip19, Relay, relayInit, UnsignedEvent, validateEvent, verifySignature } from 'nostr-tools';
-import { NostrUser } from './nostr-user';
 import { exec } from 'child_process';
+import { Event, getEventHash, getSignature, nip19, UnsignedEvent, validateEvent, verifySignature } from 'nostr-tools';
+import { NostrUser } from './nostr-user';
 
 export class RelaysService {
 
-  private connectionReady: Promise<void>;
-  private relays: Relay[] = [];
-
-  constructor(private relayUrlList: string[]) {
-    this.connectionReady = this.connect();
-  }
+  constructor(
+    private relayUrlList: string[]
+  ) { }
 
   async publish(user: NostrUser, message: string): Promise<void> {
     const event = this.createEvent(user, message);
@@ -32,34 +29,6 @@ export class RelaysService {
 
     const publishKey = nip19.noteEncode(event.id);
     console.info(`published: https://primal.net/e/${publishKey}`);
-  }
-
-  private connect(): Promise<void> {
-    const promises = this.relayUrlList.map(relayAddress => this.launchRelay(relayAddress));
-    this.connectionReady = Promise.all(promises).then(() => Promise.resolve());
-    return this.connectionReady;
-  }
-
-  private async launchRelay(relayAddress: string): Promise<void> {
-    console.info(`init relay: ${relayAddress}`);
-    const relay = relayInit(relayAddress);
-    relay.on('connect', () => {
-      console.log(`connected to ${relay.url}`)
-    });
-
-    relay.on('error', () => {
-      console.error(`failed to connect to ${relay.url}`)
-    });
-
-    try {
-      await relay.connect();
-    } catch {
-      //  if it does not connect I just ignore and continue
-      return Promise.resolve();
-    }
-
-    this.relays.push(relay);
-    return Promise.resolve();
   }
 
   private createEvent(user: NostrUser, message: string): Event {
